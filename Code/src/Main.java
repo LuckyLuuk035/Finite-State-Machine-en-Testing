8a19823a5d19d472640fdb61f8eb157951d6154f
LuckyLuuk035 <LuckyLuuk035@users.noreply.github.com>
2021-05-17 14:48:59 +0200

    Update Main.java

--- Code/src/Main.java
+++ Code/src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -10,9 +7,15 @@ public class Main {
     }
 
     private static void formatieveOpdracht() {
-        HashMap<String, Node> nodeList = Node.createNodes(Arrays.asList("s0", "s1", "s2", "s3"));
+        LinkedHashMap<String, Node> nodeList = Node.createNodes(Arrays.asList("s0", "s1", "s2", "s3"));
 
-        FSM fsmOpdracht = new FSM(nodeList,nodeList.get("s0"),"BBABA");
+        nodeList.get("s0").setPaths(nodeList, Arrays.asList("s2", "s1"));
+        nodeList.get("s1").setPaths(nodeList, Arrays.asList("s1", "s2"));
+        nodeList.get("s2").setPaths(nodeList, Arrays.asList(null, "s3"));
+        nodeList.get("s3").setPaths(nodeList, Arrays.asList("s3", "s0"));
+
+
+        FSM fsmOpdracht = new FSM(nodeList,nodeList.get("s0"),"BAABA");
         System.out.println(fsmOpdracht.run());
     }
 }
