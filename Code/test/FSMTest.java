d939f0f9b250dfb3f377e0c76572fa8888228288
LuckyLuuk035 <LuckyLuuk035@users.noreply.github.com>
2021-05-31 20:35:25 +0200

    Update FSMTest.java

--- Code/test/FSMTest.java
+++ Code/test/FSMTest.java
@@ -28,12 +28,18 @@ class FSMTest {
         String input = "BAAB"; // De input van het voorbeeld.
         List<String> result = Arrays.asList("s0", "s1", "s1", "s1", "s2"); // Het resultaat van het voorbeeld.
         assertEquals(result, Main.formatieveOpdracht("s0",input));
-        if (result.equals(Main.formatieveOpdracht("s0",input))); { System.out.println("Het voorbeeld van de Formatieve Opdracht geeft het zelfde resultaat"); }
+        if (result.equals(Main.formatieveOpdracht("s0",input))); {
+            System.out.println("Het voorbeeld van de Formatieve Opdracht geeft het zelfde resultaat");
+        }
 
 
-        //
-        // check of de FSM daadwerkelijk stopt als hij een a ontvangt bij s2.
+        // Check of de FSM daadwerkelijk stopt als hij een a ontvangt bij 's2'.
         int test = Main.formatieveOpdracht("s2","ABABABABA").size();
+        // Aangezien we beginnen bij 's2' wordt er verwacht dat de grote van de list 1 is (namelijk alleen s2).
         assertEquals(1, test);
+        if(test == 1); {
+            System.out.println("De FSM stopt bij een input van B op s2");
+        }
+
     }
 }
